adfdf43330efadcc085c96223064bb4fb3da44b4
AATataj <muresan44@gmail.com>
2020-02-26 15:26:08 -0500

    created generalized fn() that will add new feature series to existing features db

--- features.py
+++ features.py
@@ -6,6 +6,7 @@ import pathlib
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+from sqlalchemy import create_engine
 
 ## this file is meant to be an example of utilizing 
 ## pandas to calculate my day-by-day feature vectors via pandas.dataFrame()
@@ -13,6 +14,7 @@ import pandas as pd
 ## awfully redundant, as well as giving away all my best ideas publicly
 
 def pointsVector(cnx):
+        cursor = cnx.cursor()
         ## necessary data for calculated features
         dataFrame = pd.read_sql_query("select name, playerID from boxscores limit 20", cnx)
         series = pd.read_sql_query("select date from boxscores limit 20", cnx)
@@ -21,7 +23,7 @@ def pointsVector(cnx):
         dataFrame2 = pd.concat([dataFrame, series, pointsSeries], axis = 1)
 
         avg7Days = pd.Series()
-        #calculate features for each player/date
+        ##calculate features for each player/date
         for row in range(len(dataFrame2)):
                 query = """
                         select avg(pts)
@@ -36,4 +38,35 @@ def pointsVector(cnx):
 
         ## append calculated feature series to basic dataframe
         dataFrame2['ptsAvg7Days'] = avg7Days
-        print (dataFrame2.head())
+
+        ## cast date column to a datetime object
+        dataFrame2['date'] = pd.to_datetime(dataFrame2['date'], format = '%Y-%m-%d')
+        
+        #cursor.execute("drop table inputvectors")
+        colNames = []
+        colTypes = []
+        for col in dataFrame2.columns:
+                colNames.append(col)
+                if dataFrame2[col].dtype == 'object':
+                        colTypes.append('varchar(150)')
+                if dataFrame2[col].dtype == 'int64':
+                        colTypes.append('int')
+                if dataFrame2[col].dtype == 'datetime64[ns]':
+                        colTypes.append('date')
+                if dataFrame2[col].dtype == 'float64':
+                        colTypes.append('decimal(7,4)')
+                
+        query = """
+                        create table inputvectors (
+                        """
+        for i in range(len(colNames)):
+                query += colNames[i] + " " + colTypes[i] + ',' 
+        query = query[:-1]
+        query += ')'
+        cursor.execute(query)
+
+
+        engine = create_engine("mysql://slick:muresan44@localhost/nba")
+        con = engine.connect()
+        dataFrame2.to_sql(name='inputvectors',index=False, con=con, if_exists='append')
+        
