774b0f8afafca41cd037a4ac4b901d33fbc3f001
AATataj <muresan44@gmail.com>
2020-05-24 16:53:49 -0400

    progress on filtering out NaNs

--- linearRegression.py
+++ linearRegression.py
@@ -29,12 +29,15 @@ def linReg(cnx) :
 
     ## partition into the labels and features
     labels = unfiltered.pts
-    features = unfiltered.drop(["index", "name", "date", "playerID", "fgPer", "ftPer", "pts", "3fgm", "trb", 
+    features = unfiltered.drop(["index", "name", "playerID", "fgPer", "ftPer", "pts", "3fgm", "trb", 
     "ast", "stl", "blk", "tov"], axis=1)
 
-    print(features.isnull().sum())
-    pdb.set_trace()
+    removePtsNaN(features)
 
+    # drop date afterwards
+    features = features.drop(['date'], axis=1)
+    print(features.isnull().sum())
+    """
     ## normalize feature values to between 0..1:
 
     ## by standard deviation (handles outliers better):
@@ -82,7 +85,7 @@ def linReg(cnx) :
     
     hist = pd.DataFrame(history.history)
     hist['epoch'] = history.epoch
-    print(hist)
+    print(hist)"""
 
     print ("linReg() complete.")
 
@@ -90,3 +93,56 @@ def linReg(cnx) :
     #      : 1st game of the season every season will yield a NaN value for all avg not career
     ##     : 1st game back from injury extending beyond 7 days will yield NaN for 7 days, same for 14, 30, etc
     ##     : need to decide how to handle these NaN values in the data, and update the dataframe (not the DB)
+
+def removePtsNaN(features):
+    
+    # set any nan to last 30 days avg of last non-nan
+    print (features.head())
+    for row in features.index:
+        #y=False
+        for x in range(1, len(features.loc[row])):
+            #print(features.loc[row][x])
+            if np.isnan(features.loc[row][x]) :
+                #y=True
+                last30date = max_lt(features['date'], features.loc[row, 'date'])
+                nonNaN = features.loc[features['date']==last30date]
+                
+                #print(last30date)
+                #print(features.loc[row]['date'])
+                print(nonNaN)
+                print(type(nonNaN))
+                print(nonNaN.shape)
+                pdb.set_trace()
+                #print(type(nonNaN.get(key='ptsAvg30Days')))
+                #print(nonNaN['ptsAvg30Days'].shape)
+                print('----')
+                
+                #row[x] = 
+
+        #if y:
+            ## find max date without nan less than current row
+            """last30date = max_lt(features['date'], row[1][0])
+            
+            nonNaN = features.loc[features['date']==last30date]
+            print (nonNaN)
+            print (row[1]['date'])            
+            print (last30date)
+            print('-----')
+            """
+def max_lt(seq, val):
+    """
+    Return greatest item in seq for which item < val applies.
+    None is returned if seq was empty or all items in seq were >= val.
+
+    >>> max_lt([3, 6, 7, 11], 10)
+    7
+    >>> max_lt((5, 9, 12, 13), 12)
+    9
+    """
+
+    idx = len(seq)-1
+    while idx >= 0:
+        if seq[idx] < val:
+            return seq[idx]
+        idx -= 1
+    return None
