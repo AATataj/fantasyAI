7eecac8df967d9c00bf117d72ec73c89867cdd0d
AATataj <muresan44@gmail.com>
2020-11-21 01:56:48 -0500

    leetcode and sorting algorithm practice

--- scratch work/practice.py
+++ scratch work/practice.py
@@ -155,3 +155,12 @@ def bubbleSort(arr):
         swap=False
     return arr
 
+# insertion sort complexity n^2
+def insertionSort(arr):
+    for i in range(1, len(arr)):
+        for j in range(i):
+            if arr[i]< arr[j]:
+                temp = arr[i]
+                arr[i] = arr[j]
+                arr[j]=temp
+    return arr
